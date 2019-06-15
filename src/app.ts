2ca3b223abe2a2dab48f42ef670a8168095909ae
Edilson Pacheco <edilsopacheco6@gmail.com>
2019-06-15 15:32:06 -0300

    first node application with Typescript

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+class App {
+    public express: express.Application
+
+    public constructor () {
+      this.express = express()
+
+      this.middlewares()
+      this.routes()
+      this.database()
+    }
+
+    private middlewares (): void{
+      this.express.use(express.json())
+      this.express.use(cors())
+    }
+
+    private database (): void {
+      mongoose.connect('mongodb://192.168.99.100:27012/ts-users', {
+        useNewUrlParser: true
+      })
+    }
+
+    private routes (): void{
+      this.express.get('/', (req, res) => {
+        return res.send('Hellow World')
+      })
+    }
+}
+export default new App().express
