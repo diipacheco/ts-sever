c2ef69dd94a15d855a2563e6bd575e7e9cc45bf6
Edilson Pacheco <edilsopacheco6@gmail.com>
2019-06-17 11:46:51 -0300

    use route instance in the main class

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import routes from './routes'
 
 class App {
     public express: express.Application
@@ -24,10 +25,8 @@ class App {
       })
     }
 
-    private routes (): void{
-      this.express.get('/', (req, res) => {
-        return res.send('Hellow World')
-      })
+    private routes (): void {
+      this.express.use(routes)
     }
 }
 export default new App().express
